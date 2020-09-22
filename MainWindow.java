f139bdff00e9f6ae95b0aa9efa50ba51243e3e10
Fernando Pérez <71733128+fernandoperezufv@users.noreply.github.com>
2020-09-22 23:55:31 +0200

    Adaptación a la rama "Beta"
    Se ha adaptado el fichero anterior para esta rama

--- MainWindow.java
+++ MainWindow.java
@@ -1,6 +1,6 @@
 public class MainWindow {
     public static void main(String[] args) {
-        System.out.println("Git Branch: Master");
+        System.out.println("Git Branch: Beta");
 
         for (int i = 0; i < 5; i++) {
             System.out.println("(" + i + ") Hello, World!");
